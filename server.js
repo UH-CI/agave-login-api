1a0e028177e0828af67ed60b83a525147db8254e
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2017-10-02 13:48:16 -0600

    added token refresh
    added token refresh at /refresh?refresh_token=SomeToken

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // server.js
 const express        = require('express');
-var cors = require('cors')	
+var cors = require('cors')
 const bodyParser     = require('body-parser');
 const app            = express();
 
 app.use(cors());
 const consumerKey ="ar6syIISmJsmxFtayKzxoo27C44a";
 const consumerSecret ="b70mKZmRDJlsBGgxbgDaBDMNy14a";
+
+var white_list = ['seanbc'];
 const port = 8000;
 const uri ="agaveauth.its.hawaii.edu";
 var querystring = require('querystring');
@@ -32,46 +34,85 @@ app.get('/', function (req, res) {
   res.send('GET request to the homepage')
 })
 
-// POST to login and return the tokens and referesh token
+// POST to login and return the tokens and refresh token
 app.post('/login', function (req, res)  {
-   var results = '';
-   var data = querystring.stringify({ grant_type: "password", username: req.query.username, password: req.query.password,
-        scope:"PRODUCTION", callbackUrl: "http://localhost:8000" });
-//  console.log(data)
+   if (white_list.indexOf(req.query.username) > -1){
+     var data = querystring.stringify({ grant_type: "password", username: req.query.username, password: req.query.password,
+          scope:"PRODUCTION", callbackUrl: "http://localhost:8000" });
+  //  console.log(data)
 
-   var auth_string = consumerKey + ':' + consumerSecret;
-   var b64_auth_string = 'Basic ' + Buffer.from(auth_string).toString('base64');//.toString('base64');
+     var auth_string = consumerKey + ':' + consumerSecret;
+     var b64_auth_string = 'Basic ' + Buffer.from(auth_string).toString('base64');//.toString('base64');
 
-   var options = {
-      rejectUnauthorized: false,
-      hostname: uri,
-      port: 443,
-      path: '/token',
-      method: 'POST',
-      headers: {'Authorization': b64_auth_string, 'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(data)}
-    };
+     var options = {
+        rejectUnauthorized: false,
+        hostname: uri,
+        port: 443,
+        path: '/token',
+        method: 'POST',
+        headers: {'Authorization': b64_auth_string, 'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(data)}
+      };
 
-    var request =  https.request(options, function(response) {
-      console.log('Status: ' + res.statusCode);
-      console.log('Headers: ' + JSON.stringify(res.headers));
-      response.setEncoding('utf8');
-      response.on('data', function (body) {
-        console.log('Body: ' + body);
-        results=body;
-	res.send(results)
+      var request =  https.request(options, function(response) {
+        console.log('Status: ' + res.statusCode);
+        console.log('Headers: ' + JSON.stringify(res.headers));
+        response.setEncoding('utf8');
+        response.on('data', function (body) {
+          console.log('Body: ' + body);
+  	      res.send(body)
+        });
+        response.on('error', function(e) {
+          console.log('problem with request: ' + e.message);
+          res.send(e)
+        });
       });
-      response.on('error', function(e) {
-        console.log('problem with request: ' + e.message);
-        res.send(e)
-      });
-    });
-
-    request.write(data);
-
-
+      request.write(data);
+    }
+    else{
+      res.send("{'error':'Incorrect Login Credentials or Permissions.'}")
+    }
 })
 
+// POST to refresh and return the tokens and refresh token
+app.post('/refresh', function (req, res)  {
+   if (white_list.indexOf(req.query.username) > -1){
+     var data = querystring.stringify({ grant_type: "refresh_token", refresh_token: req.query.refresh_token,
+          scope:"PRODUCTION", callbackUrl: "http://localhost:8000" });
+  //  console.log(data)
+
+     var auth_string = consumerKey + ':' + consumerSecret;
+     var b64_auth_string = 'Basic ' + Buffer.from(auth_string).toString('base64');//.toString('base64');
+
+     var options = {
+        rejectUnauthorized: false,
+        hostname: uri,
+        port: 443,
+        path: '/token',
+        method: 'POST',
+        headers: {'Authorization': b64_auth_string, 'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(data)}
+      };
+
+      var request =  https.request(options, function(response) {
+        console.log('Status: ' + res.statusCode);
+        console.log('Headers: ' + JSON.stringify(res.headers));
+        response.setEncoding('utf8');
+        response.on('data', function (body) {
+          console.log('Body: ' + body);
+  	      res.send(body)
+        });
+        response.on('error', function(e) {
+          console.log('problem with request: ' + e.message);
+          res.send(e)
+        });
+      });
+      request.write(data);
+    }
+    else{
+      res.send("{'error':'Incorrect Credentials.'}")
+    }
+})
 /*app.listen(port, () => {
   console.log('We are live on ' + port);
 });*/
